34ff346b2cf7b0beff2a3e4b91b92f840b961a6f
guivn <guivnz@gmail.com>
2015-11-07 19:04:43 -0200

    Removendo codigo inutil

--- sentiment_analysis/sentiment_analysis.py
+++ sentiment_analysis/sentiment_analysis.py
@@ -56,69 +56,8 @@ f = open('obj/'+ 'dict' + '.pkl', 'wb')
 
 full_dict = make_full_dict
 
-
 #tries using all words as the feature selection mechanism
 print 'using all words as features'
 evaluate_features(full_dict)
-
-#scores words based on chi-squared test to show information gain (http://streamhacker.com/2010/06/16/text-classification-sentiment-analysis-eliminate-low-information-features/)
-def create_word_scores():
-	#creates lists of all positive and negative words
-	posWords = []
-	negWords = []
-	with open(RT_POLARITY_POS_FILE, 'r') as posSentences:
-		for i in posSentences:
-			posWord = re.findall(r"[\w']+|[.,!?;]", i.rstrip())
-			posWords.append(posWord)
-	with open(RT_POLARITY_NEG_FILE, 'r') as negSentences:
-		for i in negSentences:
-			negWord = re.findall(r"[\w']+|[.,!?;]", i.rstrip())
-			negWords.append(negWord)
-	posWords = list(itertools.chain(*posWords))
-	negWords = list(itertools.chain(*negWords))
-
-	#build frequency distibution of all words and then frequency distributions of words within positive and negative labels
-	word_fd = FreqDist()
-	cond_word_fd = ConditionalFreqDist()
-	for word in posWords:
-		word_fd[word.lower()] += 1
-		cond_word_fd['pos'][word.lower()] += 1
-	for word in negWords:
-		word_fd[word.lower()] += 1
-		cond_word_fd['neg'][word.lower()] += 1
-
-	#finds the number of positive and negative words, as well as the total number of words
-	pos_word_count = cond_word_fd['pos'].N()
-	neg_word_count = cond_word_fd['neg'].N()
-	total_word_count = pos_word_count + neg_word_count
-
-	#builds dictionary of word scores based on chi-squared test
-	word_scores = {}
-	for word, freq in word_fd.iteritems():
-		pos_score = BigramAssocMeasures.chi_sq(cond_word_fd['pos'][word], (freq, pos_word_count), total_word_count)
-		neg_score = BigramAssocMeasures.chi_sq(cond_word_fd['neg'][word], (freq, neg_word_count), total_word_count)
-		word_scores[word] = pos_score + neg_score
-
-	return word_scores
-
-#finds word scores
-word_scores = create_word_scores()
-
-#finds the best 'number' words based on word scores
-def find_best_words(word_scores, number):
-	best_vals = sorted(word_scores.iteritems(), key=lambda (w, s): s, reverse=True)[:number]
-	best_words = set([w for w, s in best_vals])
-	return best_words
-
-#creates feature selection mechanism that only uses best words
-def best_word_features(words):
-	return dict([(word, True) for word in words if word in best_words])
-
-#numbers of features to select
-numbers_to_test = [15000]
-#tries the best_word_features mechanism with each of the numbers_to_test of features
-for num in numbers_to_test:
-	best_words = find_best_words(word_scores, num)
-	evaluate_features(best_word_features)
 
 f.close()
