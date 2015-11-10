c4343a9273970fb8c50d8f670804d5f1d4c22626
guivn <guivnz@gmail.com>
2015-11-10 00:38:14 -0200

    Preproc Stopwords

--- sentiment_analysis/sentiment_analysis.py
+++ sentiment_analysis/sentiment_analysis.py
@@ -2,26 +2,35 @@ import re, math, collections, itertools, os
 import nltk, nltk.classify.util
 import pickle
 from nltk.classify import NaiveBayesClassifier
+from nltk.corpus import stopwords
 
 POLARITY_DATA_DIR = os.path.join('polarityData', 'rt-polaritydata')
 RT_POLARITY_POS_FILE = os.path.join(POLARITY_DATA_DIR, 'rt-polarity-pos.txt')
 RT_POLARITY_NEG_FILE = os.path.join(POLARITY_DATA_DIR, 'rt-polarity-neg.txt')
 
-
+stop = stopwords.words('english')
 #this function takes a feature selection mechanism and returns its performance in a variety of metrics
 def evaluate_features(feature_select):
 	posFeatures = []
 	negFeatures = []
+
 	#http://stackoverflow.com/questions/367155/splitting-a-string-into-words-and-punctuation
 	#breaks up the sentences into lists of individual words (as selected by the input mechanism) and appends 'pos' or 'neg' after each list
 	with open(RT_POLARITY_POS_FILE, 'r') as posSentences:
 		for i in posSentences:
 			posWords = re.findall(r"[\w']+|[.,!?;]", i.rstrip())
+			for i in posWords:
+				if i in stop:
+					posWords.remove(i)
 			posWords = [feature_select(posWords), 'pos']
 			posFeatures.append(posWords)
+
 	with open(RT_POLARITY_NEG_FILE, 'r') as negSentences:
 		for i in negSentences:
 			negWords = re.findall(r"[\w']+|[.,!?;]", i.rstrip())
+			for i in negWords:
+				if i in stop:
+					negWords.remove(i)
 			negWords = [feature_select(negWords), 'neg']
 			negFeatures.append(negWords)
 
@@ -40,7 +49,7 @@ def evaluate_features(feature_select):
 
 #creates a feature selection mechanism that uses all words
 def make_full_dict(words):
-	word_dict = dict([(word, True) for word in words])	
+	word_dict = dict([(word, True) for word in words if word not in stop])	
 	#print word_dict
 	save_obj(word_dict, 'dict')
 	return word_dict
