d3e96925510bcb24bed76ce7b523cfed8b1fae64
guivn <guivnz@gmail.com>
2015-11-09 22:48:42 -0200

    Input e output por json

--- sentiment_analysis/classify.py
+++ sentiment_analysis/classify.py
@@ -1,17 +1,10 @@
-import re, math, collections, itertools, os
-import nltk, nltk.classify.util, nltk.metrics
-import pickle
+import re, math
+import nltk, nltk.classify.util
 from nltk.classify import NaiveBayesClassifier
-from nltk.metrics import BigramAssocMeasures
-from nltk.probability import FreqDist, ConditionalFreqDist
+import pickle
+import json
 
-POLARITY_DATA_DIR = os.path.join('polarityData', 'rt-polaritydata')
-RT_POLARITY_TEST_FILE = os.path.join(POLARITY_DATA_DIR, 'test.txt')
-
-# def load_obj(name ):
-#     with open('obj/' + name + '.pkl', 'rb') as f:
-#         return pickle.load(f)
-def load_obj(name):
+def load_dict(name):
 	objs = dict()
 	with open('obj/' + name + '.pkl', 'rb') as f:
 		while 1:
@@ -20,38 +13,42 @@ def load_obj(name):
 			except EOFError:
 				break
 	f.close()
-	#print objs
 	return objs
 
-def classifyTweets():
-	feature_select = load_obj('dict')
+def load_tweets():
+	with open('tweets.json') as tt:    
+    		data = json.load(tt)
 
-	testFeatures = []
+    	testSentences = []
 
-	f = open('classifier.pickle', 'rb')
+    	for i in data["tweets"]:
+		testSentences.append(i["text"])
+
+	return testSentences, data
+
+def save_json(data):
+	with open('data.json', 'w') as outfile:
+    		json.dump(data, outfile)
+
+def classifyTweets(feature_select):
+
+	testSentences, data = load_tweets()
+
+	f = open('obj/classifier.pickle', 'rb')
 	classifier = pickle.load(f)
 	f.close()
 
-	with open(RT_POLARITY_TEST_FILE, 'r') as testSentences:
-		for i in testSentences:
-			testSentences = re.findall(r"[\w']+|[.,!?;]", i.rstrip())
-			testFeatures = {word: (word in testSentences) for word in feature_select}
-			#testSentences = [feature_select(testSentences), 'neg']
-			#testFeatures.append(testSentences)	
+	j = 0
 
-			predicted = classifier.classify(testFeatures)
-			print predicted
-			#print 'accuracy:', nltk.classify.util.accuracy(classifier, testFeatures)
+	for i in testSentences:
+	 	testSentences = re.findall(r"[\w']+|[.,!?;]", i.rstrip())
+		testFeatures = {word: (word in testSentences) for word in feature_select}
+		predicted = classifier.classify(testFeatures)
+		data["tweets"][j]["sentiment"] = predicted
+		j += 1
 
+	#print data
+	save_json(data)
 
-	# #initiates referenceSets and testSets
-	# referenceSets = collections.defaultdict(set)
-	# testSets = collections.defaultdict(set)	
-
-	# #puts correctly labeled sentences in referenceSets and the predictively labeled version in testsets
-	# for i, (features) in enumerate(testFeatures):
-	# 	#referenceSets[label].add(i)
-	# 	predicted = classifier.classify(features)
-	# 	testSets[predicted].add(i)
-
-classifyTweets()
+feature_select = load_dict('dict')
+classifyTweets(feature_select)
