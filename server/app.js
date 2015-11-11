c87169005afc61462757eb8e1033d3519ce2f572
Willian Policiano <w.policiano@gmail.com>
2015-11-11 04:44:17 -0200

    Componenete para rodar script Python Criado

--- server/app.js
+++ server/app.js
@@ -22,6 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/scripts', express.static(__dirname + '/scripts/'));
 
 app.use('/', routes);
 app.use('/users', users);
--- server/routes/twitter.js
+++ server/routes/twitter.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var prettyjson = require('prettyjson');
+var PythonShell = require('python-shell');
+var path = require('path');
 var twitterController = require('../controllers/twitterController.js');
 var router = express.Router();
 
@@ -7,10 +9,11 @@ var router = express.Router();
 router.get('/:query', function(req, res, next) {
 
 	twitterController.list(req, function(response){
+		python();
 		var options = {
-		  noColor: false
+			noColor: false
 		};
-		console.log(prettyjson.render(response, options));
+		//console.log(prettyjson.render(response, options));
 
 		var string = "<p><b>Assunto:</b>"+req.params.query+ "<br>"+response.length+" resultados georeferenciados";
 		for(var i = 0; i < response.length; i ++){
@@ -21,5 +24,23 @@ router.get('/:query', function(req, res, next) {
 	});
 
 });
+
+var python = function() {
+
+	PythonShell.defaultOptions  = {
+		scriptPath: path.join(__dirname, 'scripts'),
+		args: ['value1', 'value2', 'value3']
+	};
+
+	PythonShell.run('test.py', function(err, result){
+		if (err) throw err;
+		//var json = JSON.parse(result);
+		console.log(result);
+	});
+
+	// receive a message in JSON mode
+
+
+}
 
 module.exports = router;
