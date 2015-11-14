f3b916441d6f632f702948927295f126c5748178
Willian Policiano <w.policiano@gmail.com>
2015-11-14 09:05:05 -0200

    Intersection lib added

--- loopback/server/boot/root.js
+++ loopback/server/boot/root.js
@@ -1,24 +1,30 @@
 var colors = require('colors/safe');
-
+var inside = require('point-in-polygon');
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
   
   router.get('/', server.loopback.status());
 
-  router.get('/tweeter/:query', function(req, res) {
+  router.get('/twitter/:query', function(req, res) {
   	var query = req.params.query;
 
   	var Tweet = server.models.Tweet;
-  	
-  	console.log(colors.blue('uberlandia: ') + "query: "+ query);
 
-  	Tweet.find({texto:query}, function(err, response){
+    var polygon = [ [ 0, 0 ], [ -2, -2 ], [ 0, -2 ], [ 2, 0 ] ];
+     
+    console.dir([
+        inside([ -1, -1 ], polygon),
+        inside([ 4.9, 1.2 ], polygon),
+        inside([ 1.8, 1.1 ], polygon)
+    ]);
+    
+  	//Tweet.find({texto:query}, function(err, response){
   		
-    	console.log(colors.blue('uberlandia: ') + "response: ");
-    	console.log(response);
-    	res.send(response);
-  	});
+    	//console.log(colors.blue('uberlandia: ') + "response: ");
+    	//console.log(response);
+    	
+  	//});
     
     
   });
@@ -26,3 +32,4 @@ module.exports = function(server) {
 
   server.use(router);
 };
+
