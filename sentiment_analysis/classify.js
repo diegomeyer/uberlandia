ca8ae91eec8ab9efde40538044a8753e444b539d
guivn <guivnz@gmail.com>
2015-11-30 20:47:20 -0200

    classificador como modulo

--- sentiment_analysis/classify.js
+++ sentiment_analysis/classify.js
@@ -1,29 +1,35 @@
 var MsTranslator = require('mstranslator');
 var bayes = require('bayes')
 
-var stateJson = require('./naivebayes.json');
-var classifier = bayes.fromJson(stateJson)
+var classifyArray = function classifyArray(input_array, lang, translateTo, callback) {
+    
+    var stateJson = require('./naivebayes.json');
+    var classifier = bayes.fromJson(stateJson);
 
-var client = new MsTranslator({
-  client_id: "uberlandia"
-  , client_secret: "MlqN4nijjPlGfXyZmuYZB5jW3/GmDGKUWLGosuQj9LY="
-}, true);
+    var client = new MsTranslator({
+       client_id: "uberlandia"
+      , client_secret: "MlqN4nijjPlGfXyZmuYZB5jW3/GmDGKUWLGosuQj9LY="
+    }, true);
 
-var tweets = ["Amo", "odeio", "vc é louco cachoeira"];
+    var params = {
+      texts: input_array
+      , from: lang
+      , to: translateTo
+    };
 
-var params = {
-  texts: tweets//'tem que ser retardado pra gostar do bolsonaro'
-  , from: 'pt-br'
-  , to: 'en'
-};
+    var data;
+    var resultado = new Array();
 
-var data;
-var resultado = [];
-// // Don't worry about access token, it will be auto-generated if needed.
-client.translateArray(params, function(err, data) {
-	data.forEach(function(tweet) {
-		console.log(tweet.TranslatedText);
-		console.log(classifier.categorize(String(tweet.TranslatedText)))
-		resultado.push(classifier.categorize(String(tweet.TranslatedText)))
-	});
-});
+    client.translateArray(params, function(err, data) {
+        data.forEach(function(phrase) {
+            console.log(phrase.TranslatedText);
+            console.log(classifier.categorize(String(phrase.TranslatedText)));
+            resultado.push(classifier.categorize(String(phrase.TranslatedText)));
+        });
+
+        callback(resultado);
+    });
+
+}
+
+exports.classifyArray = classifyArray;
